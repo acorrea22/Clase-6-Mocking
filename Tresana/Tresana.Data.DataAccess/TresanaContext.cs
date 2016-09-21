753fb766a7d6957d7dd67d45d1984a957a9c4114
Gabriel <piffarettig@gmail.com>
2016-09-21 19:41:11 -0300

    added project

---
+++ Tresana/Tresana.Data.DataAccess/TresanaContext.cs
@@ -0,0 +1,12 @@
+﻿using Tresana.Data.Entities;
+using System.Data.Entity;
+
+namespace Tresana.Data.DataAccess
+{
+    public class TresanaContext : DbContext
+    {
+        public TresanaContext():base("name=TresanaContext") { }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ Tresana/Tresana.Data/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Tresana.Data.Entities
+{
+    public class User 
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public string Address { get; set; }
+        public virtual IEnumerable<Task> Tasks { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
